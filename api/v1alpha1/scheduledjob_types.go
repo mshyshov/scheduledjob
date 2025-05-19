ee3f79441a27b75daf4d4cb8cb1778a7e270b8ff
Mykhailo Shyshov <msshyshov@gmail.com>
2025-05-19 20:47:43 +0200

    Basic CRD definitions

--- api/v1alpha1/scheduledjob_types.go
+++ api/v1alpha1/scheduledjob_types.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1alpha1
 
 import (
+	v1 "k8s.io/api/batch/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -28,20 +29,67 @@ type ScheduledJobSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of ScheduledJob. Edit scheduledjob_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// The schedule in Cron format, see https://en.wikipedia.org/wiki/Cron
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
+	Schedule string `json:"schedule"`
+
+	// The time zone name for the given schedule, see https://en.wikipedia.org/wiki/List_of_tz_database_time_zones.
+	// If not specified, this will default to the time zone of the kube-controller-manager process.
+	// The set of valid time zone names and the time zone offset is loaded from the system-wide time zone
+	// database by the API server during ScheduledJob validation and the controller manager during execution.
+	// If no system-wide time zone database can be found a bundled version of the database is used instead.
+	// If the time zone name becomes invalid during the lifetime of a ScheduledJob or due to a change in host
+	// configuration, the controller will stop creating new new Jobs and will create a system event with the
+	// reason UnknownTimeZone.
+	// More information can be found in https://kubernetes.io/docs/concepts/workloads/controllers/cron-jobs/#time-zones
+	// +optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
+	TimeZone *string `json:"timeZone,omitempty"`
+
+	// This flag tells the controller to suspend subsequent executions, it does
+	// not apply to already started executions.  Defaults to false.
+	// +optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
+	Suspend *bool `json:"suspend,omitempty"`
+
+	// Optional deadline in seconds for starting the job if it misses scheduled
+	// time for any reason.  Missed jobs executions will be counted as failed ones.
+	// +optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
+	StartingDeadlineSeconds *int64 `json:"startingDeadlineSeconds,omitempty"`
+
+	// Specifies the job that will be created when executing a ScheduledJob.
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
+	JobTemplate v1.JobTemplateSpec `json:"jobTemplate"`
+
+	// The number of successful finished jobs to retain. Value must be non-negative integer.
+	// Defaults to 1.
+	// +optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
+	SuccessfulJobsHistoryLimit *int32 `json:"successfulJobsHistoryLimit,omitempty"`
+
+	// The number of failed finished jobs to retain. Value must be non-negative integer.
+	// Defaults to 1.
+	// +optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
+	FailedJobsHistoryLimit *int32 `json:"failedJobsHistoryLimit,omitempty"`
 }
 
 // ScheduledJobStatus defines the observed state of ScheduledJob
 type ScheduledJobStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+
+	// Conditions store the status conditions of created Jobs
+	// +operator-sdk:csv:customresourcedefinitions:type=status
+	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
 }
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
 
 // ScheduledJob is the Schema for the scheduledjobs API
+// +kubebuilder:subresource:status
 type ScheduledJob struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
